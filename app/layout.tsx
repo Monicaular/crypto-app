83dc62973ed91828e72eac3b8abc46ba872a3f32
Monicaular <moniq.iancu@gmail.com>
2026-02-22 09:48:24 +0000

    Add basic links to navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +29,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
        <div>
-        
+        <Link href="/">Home</Link>
+        <Link href="/portfolio">Portfolio</Link>
        </div>
         {children}
       </body>
