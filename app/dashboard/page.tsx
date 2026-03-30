f656d4b8107db03c9f0f4df28379a9fb239858f0
Monicaular <moniq.iancu@gmail.com>
2026-03-30 07:37:50 +0100

    Updated coin list fetch

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import { HomeSubnav } from "../components/home/HomeSubnav";
+import Coins from "../components/coins/Coins";
 
 export default function Dashboard() {
   return (
     <div className="flex min-h-screen flex-col bg-zinc-50 font-sans box-border dark:bg-black">
   <HomeSubnav />
-  
+  <Coins />
       <main className="flex min-h-screen w-full max-w-3xl  py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
      
