85ff35d75ed20f42d2715b9bee380594c50365c5
Monicaular <moniq.iancu@gmail.com>
2026-04-19 15:43:01 +0100

    Update search bar to use /search endpoint

--- app/components/navbar/SearchBar.tsx
+++ app/components/navbar/SearchBar.tsx
@@ -1,18 +1,33 @@
 "use client";
 
-import { useAppSelector } from "@/state/hooks";
-import { selectFilteredCoins } from "@/store/coinsSelector";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getSearchResults } from "@/utils/getSearchResults";
+import Link from "next/link";
+import { SearchCoin } from "@/types/searchCoin";
 
 export const SearchBar = () => {
 
     const [query, setQuery] = useState("");
+    const [results, setResults] = useState<SearchCoin[]>([]);
 
-    const filtered = useAppSelector((state) => selectFilteredCoins(state, query));
+    function clearSearch() {
+        setQuery("");
+        setResults([]);
+    }
 
-    const router = useRouter();
+    useEffect(() => {
+      
+        const timeout = setTimeout(async () => {
+              if (!query) {
+            setResults([]);
+            return;
+        }
+            const data = await getSearchResults(query);
+            setResults(data);
+        }, 300);
+        return () => clearTimeout(timeout);
+    }, [query]);
 
     return(
         <div className="relative w-full max-w-md">
@@ -24,28 +39,28 @@ export const SearchBar = () => {
         className="w-full px-3 py-2 rounded-md border"
         />
 
-        {query && filtered.length > 0 && (
-            <ul className="absolute left-0 right-0 mt-2 border bg-zinc-900 rounded-md max-h-60 overflow-y-auto">
-                {filtered.map((coin) => (
-                    <li
+        {results.length > 0 && (
+            <div className="absolute left-0 right-0 mt-2 border bg-zinc-900 rounded-md max-h-60 overflow-y-auto">
+                {results.map((coin) => (
+                    <Link
                     key={coin.id}
-                    onClick={() => {router.push(`/coins/${coin.id}`);}}
+                    href={`/coins/${coin.id}`}
+                    onClick={clearSearch}
                     className="flex items-center gap-2 px-3 py-2 hover:bg-zinc-800 cursor-pointer">
                     <Image
-                    src={coin.image}
+                    src={coin.thumb}
                     alt={coin.name}
                     width={20}
                     height={20}
                     />
                     <span>
                         {coin.name}{" "}
-                        <span className="text-zinc-400">({coin.symbol.toUpperCase()})</span>
+                        <span className="text-zinc-400">({coin.symbol})</span>
                     </span>
-                    </li>
+                    </Link>
                 ))}
-            </ul>
+            </div>
         )}
         </div>
-        
     );
 };
