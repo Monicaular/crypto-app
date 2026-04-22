4b151f308eaa68ae63b3833927f5e83f262fbd00
Monicaular <moniq.iancu@gmail.com>
2026-04-22 14:28:39 +0100

    Implement markets table layout with pricing and percentage columns

--- app/components/coins/Coins.tsx
+++ app/components/coins/Coins.tsx
@@ -8,30 +8,31 @@ import Link from "next/link";
 
 import { fetchCoins } from "@/store/coinsSlice";
 import { CoinType } from "@/types/coin";
+import CoinsTable from "./CoinsTable";
 
 export default function Coins() {
-    const dispatch = useAppDispatch();
-    const { coins, loading, error} = useAppSelector((state) => state.coins);
+  const dispatch = useAppDispatch();
+  const { coins, loading, error } = useAppSelector((state) => state.coins);
 
-    useEffect(() => {
-        dispatch(fetchCoins());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchCoins());
+  }, [dispatch]);
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p className="text-red-500">{error}</p>;
-    
-    return (
-       <div className="flex flex-col gap-4">
-        {coins.map((coin: CoinType) => (
-            <Link href={`/coins/${coin.id}`} key={coin.id}>
-            
-            <div key={coin.id} className="flex items-center gap-4">
-                <Image src={coin.image} alt={coin.name} width={32} height={32}/>
-                <span>{coin.name}</span>
-                <span>${coin.current_price}</span>
-            </div>
-            </Link>
-        ))}
-       </div>
-    );
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p className="text-red-500">{error}</p>;
+
+  return (
+    <div className="p-4">
+      <CoinsTable coins={coins} />
+      {coins.map((coin: CoinType) => (
+        <Link href={`/coins/${coin.id}`} key={coin.id}>
+          <div key={coin.id} className="flex items-center gap-4">
+            <Image src={coin.image} alt={coin.name} width={32} height={32} />
+            <span>{coin.name}</span>
+            <span>${coin.current_price}</span>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
