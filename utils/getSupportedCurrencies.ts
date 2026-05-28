9ef9bf0f128689697af7ca476146752ec6f017d0
Monicaular <moniq.iancu@gmail.com>
2026-05-28 18:06:21 +0100

    Filter supported currencies to fiat only

--- utils/getSupportedCurrencies.ts
+++ utils/getSupportedCurrencies.ts
@@ -2,10 +2,35 @@ import { axiosClient } from "./axiosClient";
 import { SupportedCurrencies } from "@/types/supportedCurrencies";
 
 export async function getSupportedCurrencies(): Promise<SupportedCurrencies> {
-    const response = await axiosClient.get<SupportedCurrencies>(
-        "/simple/supported_vs_currencies"
-    );
+  const response = await axiosClient.get<SupportedCurrencies>(
+    "/simple/supported_vs_currencies",
+  );
 
-    return response.data;
-    
+  const fiatOnly = response.data.filter((cur) => [
+    "usd",
+    "eur",
+    "gbp",
+    "cad",
+    "aud",
+    "chf",
+    "jpy",
+    "cny",
+    "ron",
+    "sek",
+    "nok",
+    "dkk",
+    "pln",
+    "huf",
+    "czk",
+    "nzd",
+    "sgd",
+    "zar",
+    "inr",
+    "brl",
+    "mxn",
+    "try",
+    "rub",
+    "krw",
+  ].includes(cur));
+  return fiatOnly;
 }
