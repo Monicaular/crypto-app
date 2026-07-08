0af2173f3c1be0f6b3e975e4d5c1ef542760ee20
Monica Ular <moniq.iancu@gmail.com>
2026-07-08 15:13:03 +0100

    Add market leaderscarousel on the main page

--- utils/formatNumber.ts
+++ utils/formatNumber.ts
@@ -5,6 +5,7 @@ export function formatNumber(value: number | null | undefined) {
     return (value / 1_000_000_000_000).toFixed(2) + "T";
   if (value >= 1_000_000_000) return (value / 1_000_000_000).toFixed(2) + "B";
   if (value >= 1_000_000) return (value / 1_000_000).toFixed(2) + "M";
+  if (value >= 1_000) return (value / 1_000).toFixed(2) + "K";
 
   return value.toLocaleString();
 }
